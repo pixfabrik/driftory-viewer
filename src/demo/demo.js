f8f451f0657ee79cf78911758b1cb78546fd73f9
Daniel Tonon <dan503.npm@gmail.com>
2020-08-20 00:45:18 +1000

    Updates to demo.js

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,44 +1,40 @@
 import Driftory from '../library/driftory';
 
-var nextButton, previousButton;
-
-// ----------
-function init() {
-  var driftory = new Driftory({
+document.addEventListener('DOMContentLoaded', () => {
+  const driftory = new Driftory({
     container: document.querySelector('.driftory-viewer-container'),
     prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/'
   });
 
-  nextButton = document.querySelector('.next-button');
-  previousButton = document.querySelector('.previous-button');
+  const nextButton = document.querySelector('.next-button');
+  const previousButton = document.querySelector('.previous-button');
 
-  nextButton.addEventListener('click', function () {
-    var index = driftory.getFrameIndex();
+  nextButton.addEventListener('click', () => {
+    let index = driftory.getFrameIndex();
     index = (index + 1) % driftory.getFrameCount();
+    // TO DO: we need a built in driftory.goToNextFrame() method
     driftory.goToFrame(index);
   });
 
-  previousButton.addEventListener('click', function () {
-    var index = driftory.getFrameIndex();
+  previousButton.addEventListener('click', () => {
+    let index = driftory.getFrameIndex();
     index = index === 0 ? driftory.getFrameCount() - 1 : index - 1;
+    // TO DO: we need a built in driftory.goToPrevFrame() method
     driftory.goToFrame(index);
   });
 
   fetch('comic.json')
-    .then(function (response) {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('bad');
+        console.error(response);
+        throw new Error('Failed to load comic.json');
       }
 
       return response.json();
     })
-    .then(function (json) {
+    .then((json) => {
       // console.log(json);
-
       driftory.openComic(json.comic);
-    });
-}
-
-// ----------
-// Kick it all off!
-init();
+    })
+    .catch((error) => console.error(error));
+});
