145163401eb415990e611b1a9967a633f053a0ab
Ian Gilman <ian@iangilman.com>
2020-10-16 15:20:38 -0700

    Prettier on Comic.types

--- src/library/Comic.types.ts
+++ src/library/Comic.types.ts
@@ -1,34 +1,34 @@
 export interface Comic {
-	comic: {
-		creator: Creator,
-		body: Body
-	}
+  comic: {
+    creator: Creator;
+    body: Body;
+  };
 }
 
 export interface Creator {
-	name: string
+  name: string;
 }
 
 export interface Body {
-	formatVersion: number,
-	backgroundColor: string,
-	fadeMode: boolean,
-	items: Array<Item>,
-	frames: Array<Frame>
+  formatVersion: number;
+  backgroundColor: string;
+  fadeMode: boolean;
+  items: Array<Item>;
+  frames: Array<Frame>;
 }
 
 export interface Item {
-	x: number,
-	y: number,
-	width: number,
-	height: number,
-	url: string,
-	fileName: string
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  url: string;
+  fileName: string;
 }
 
 export interface Frame {
-	x: number,
-	y: number,
-	width: number,
-	height: number,
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
